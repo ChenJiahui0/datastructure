5a56ce247f9759716990d1e8b47a58925abe45b5
陈家辉 <876492153@qq.com>
2023-07-02 19:44:57 +0800

    update

---
+++ src/main/java/com/cjh/dsu/DSU2.java
@@ -0,0 +1,35 @@
+package com.cjh.dsu;
+
+import java.util.Arrays;
+
+/**
+ * improved with size
+ */
+public class DSU2 {
+    int[] parent;
+    int[] size;
+
+    public DSU2(int n) {
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+        Arrays.fill(size, 1);
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x), rootY = find(y);
+        if (rootX == rootY) return;
+        if (size[rootX] <= size[rootY]) {
+            parent[rootX] = rootY;
+            size[rootY] += size[rootX];
+        }else{
+            parent[rootY] = rootX;
+            size[rootX] += size[rootY];
+        }
+    }
+}
